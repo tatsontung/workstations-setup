9089486186585b4d2071bb26c0b97839b73c5458
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2021-04-22 00:04:59 +0200

    added ranger tools and cbz vim zathura

--- environments/linux/scripts/archlinux/bootstrap.sh
+++ environments/linux/scripts/archlinux/bootstrap.sh
@@ -2,6 +2,7 @@
 echo "Installation must have software for Archlinux workstation"
 yay -Syu git kitty neovim gvim zsh fish lsof nginx copyq ranger mc \
 tmux httpie tree xsel fzf fasd wget curl python python-pip \
-nfs-utils samba openssh exfatprogs ntfs-3g libsecret
+nfs-utils samba openssh exfatprogs ntfs-3g libsecret highlight atool w3m mediainfo \
+zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps
 
 sudo systemctl enable sshd
