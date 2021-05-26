588d8ebaefaa56d9b6f4f749bb0f966c33ddb487
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2021-05-27 01:07:27 +0200

    updated fonts location

--- environments/linux/scripts/commons/config.sh
+++ environments/linux/scripts/commons/config.sh
@@ -10,18 +10,18 @@ set mixer.alsa.channel=Master
 EOL
 
 echo "Installing Mono nerd fonts"
-if [ ! -d ~/.fonts ]; then
-  mkdir -p ~/.fonts
+if [ ! -d ~/.local/share/fonts ]; then
+  mkdir -p ~/.local/share/fonts
 fi
-pushd ~/.fonts
+pushd ~/.local/share/fonts
 if [ ! -d Monofur ]; then
   curl https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Monofur.zip -O -J -L
   curl https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip -O -J -L
   unzip Monofur.zip -d Monofur
   unzip Hack.zip -d Hack
-  fc-cache -fv ~/.fonts
-  rm ~/.fonts/Monofur.zip
-  rm ~/.fonts/Hack.zip
+  fc-cache -fv ~/.local/share/fonts
+  rm ~/.local/share/fonts/Monofur.zip
+  rm ~/.local/share/fonts/Hack.zip
 fi
 popd
 
