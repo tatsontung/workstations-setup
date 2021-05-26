a1bfdb382f9a17e47387065742098fa4e8cc01be
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2021-05-26 14:47:40 +0200

    added software in archlinux config

--- environments/linux/scripts/archlinux/bootstrap.sh
+++ environments/linux/scripts/archlinux/bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "Installation must have software for Archlinux workstation"
-yay -Syu git kitty neovim gvim zsh fish lsof nginx copyq ranger mc \
-tmux httpie tree xsel fzf fasd wget curl python python-pip \
-nfs-utils samba openssh exfatprogs ntfs-3g libsecret highlight atool w3m mediainfo \
+yay -Syu git kitty neovim-nightly-bin gvim zsh fish lsof nginx copyq ranger mc stow \
+tmux httpie xsel fzf fasd wget curl python python-pip libsecret highlight atool w3m mediainfo \
+nfs-utils samba openssh exfatprogs ntfs-3g \
 zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps
 
 sudo systemctl enable sshd
