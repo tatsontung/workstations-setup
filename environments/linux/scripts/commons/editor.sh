18a1e3682d8abfa43c2a861d6531a89e16b616b0
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2021-08-24 22:17:51 +0200

    added emacs in apps install

--- environments/linux/scripts/commons/editor.sh
+++ environments/linux/scripts/commons/editor.sh
@@ -4,7 +4,7 @@ echo "Installing my custom nvim fork from lunar nvim"
 rm -rf $HOME/.config/nvim
 bash <(curl -s https://raw.githubusercontent.com/tatsontung/nvim/master/utils/installer/install.sh)
 
-echo "Clone vim and config folder"
+echo "Installing my custom vim fork and config folder"
 rm -rf $HOME/.config/vim
 rm -rf $HOME/.vim
 rm $HOME/.vimrc
@@ -16,3 +16,6 @@ ln -s ~/.vim/.vimrc ~/.vimrc
 ~/.vim/bin/install
 mkdir -p ~/.vim/colors
 
+echo "Install Doom Emacs"
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
