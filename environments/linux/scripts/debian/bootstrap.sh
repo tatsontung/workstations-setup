60ac85319357902c6e38f1d5069f3e3341c538b5
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2021-08-24 22:03:50 +0200

    added fonts noto emoji for debian based

--- environments/linux/scripts/debian/bootstrap.sh
+++ environments/linux/scripts/debian/bootstrap.sh
@@ -16,7 +16,7 @@ then
     sudo apt install -y git build-essential vim vim-gtk3 neovim wget curl htop lsof tilix tmux tty-clock \
     fzf fasd zsh fish taskwarrior copyq ranger mc cmus httpie tree xsel samba-common exfat-utils \
     nginx python python3 python3-pip ruby memcached docker docker-compose python3-pip pkg-config \
-    libsecret-1-0 libsecret-1-dev atool highlight w3m mediainfo
+    libsecret-1-0 libsecret-1-dev atool highlight w3m mediainfo vim-gtk fonts-noto-color-emoji
 
     echo "Configuration git credentials gnome"
     pushd /usr/share/doc/git/contrib/credential/libsecret
@@ -35,7 +35,7 @@ else
     apt install -y git build-essential vim neovim wget curl htop lsof tilix tmux tty-clock \
     fzf fasd zsh taskwarrior copyq ranger mc cmus httpie tree xsel samba-common exfat-utils \
     nginx python python3 python3-pip ruby memcached docker docker-compose python3-pip pkg-config \
-    libsecret-1-0 libsecret-1-dev atool highlight w3m mediainfo
+    libsecret-1-0 libsecret-1-dev atool highlight w3m mediainfo vim-gtk fonts-noto-color-emoji
 
     echo "Configuration git credentials gnome"
     pushd /usr/share/doc/git/contrib/credential/libsecret
