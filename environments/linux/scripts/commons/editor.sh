a8dbdef6fa3e04831ec53ba8bb151f283a9beb38
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2021-11-03 05:49:19 +0100

    Moved to Doom NeoVim

--- environments/linux/scripts/commons/editor.sh
+++ environments/linux/scripts/commons/editor.sh
@@ -1,11 +1,11 @@
 #!/bin/env bash
 
-echo "Installing my custom nvim fork from lunar nvim"
+echo "Installing Doom NeoVim"
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 mv ./nvim.appimage $HOME/.local/bin/nvim
 rm -rf $HOME/.config/nvim
-bash <(curl -s https://raw.githubusercontent.com/tatsontung/nvim/master/utils/installer/install.sh)
+git clone --depth 1 https://github.com/NTBBloodbath/doom-nvim.git ${XDG_CONFIG_HOME:-$HOME/.config}/nvim
 
 echo "Installing my custom vim fork and config folder"
 rm -rf $HOME/.config/vim
