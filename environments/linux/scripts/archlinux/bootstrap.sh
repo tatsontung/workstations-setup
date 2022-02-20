6fed492b41a677913b6488d1b027692dcd8193ab
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2022-02-20 20:58:54 +0100

    updated script

--- environments/linux/scripts/archlinux/bootstrap.sh
+++ environments/linux/scripts/archlinux/bootstrap.sh
@@ -2,7 +2,7 @@
 echo "Installation must have software for Archlinux workstation"
 paru -Syu git kitty neovim-nightly-bin gvim emacs zsh fish lsof nginx copyq ranger mc stow \
 tmux httpie xsel fzf fasd wget curl python python-pip libsecret highlight atool w3m mediainfo \
-nfs-utils samba openssh exfatprogs zip unzip curl wget \
+nfs-utils samba openssh exfatprogs zip unzip curl wget screenfetch \
 zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps
 
 sudo systemctl enable sshd
--- environments/linux/scripts/debian/bootstrap.sh
+++ environments/linux/scripts/debian/bootstrap.sh
@@ -12,37 +12,37 @@ if [ "$(whoami)" != "root" ]
 then
     # Update all dependencies and install packages
     sudo apt update -y
-
+    
     sudo apt install -y git build-essential vim vim-gtk3 neovim wget curl htop lsof tilix tmux tty-clock \
     fzf fasd zsh fish taskwarrior copyq ranger mc cmus httpie tree xsel samba-common exfat-utils \
-    nginx python python3 python3-pip ruby memcached docker docker-compose python3-pip pkg-config \
+    nginx python python3 python3-pip ruby memcached docker docker-compose python3-pip pkg-config screenfetch \
     libsecret-1-0 libsecret-1-dev atool highlight w3m mediainfo vim-gtk fonts-noto-color-emoji emacs cmake libvterm-dev
-
+    
     echo "Configuration git credentials gnome"
     pushd /usr/share/doc/git/contrib/credential/libsecret
     sudo make
     git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/
     popd
-
+    
     # SYSCTL
     echo "setting sysctl to 9001!!!"
     # this number is for angular js watch files changes
     echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
-else 
+else
     # Update all dependencies and install packages
     apt update -y
-
+    
     apt install -y git build-essential vim neovim wget curl htop lsof tilix tmux tty-clock \
     fzf fasd zsh taskwarrior copyq ranger mc cmus httpie tree xsel samba-common exfat-utils \
-    nginx python python3 python3-pip ruby memcached docker docker-compose python3-pip pkg-config \
+    nginx python python3 python3-pip ruby memcached docker docker-compose python3-pip pkg-config screenfetch \
     libsecret-1-0 libsecret-1-dev atool highlight w3m mediainfo vim-gtk fonts-noto-color-emoji emacs cmake libvterm-dev
-
+    
     echo "Configuration git credentials gnome"
     pushd /usr/share/doc/git/contrib/credential/libsecret
     make
     git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/
     popd
-
+    
     # SYSCTL
     echo "setting sysctl to 9001!!!"
     # this number is for angular js watch files changes
