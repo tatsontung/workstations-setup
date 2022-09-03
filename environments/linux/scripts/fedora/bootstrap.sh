d0e9830d1e2f5ee3b65a329f7b12bba382a8250e
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2022-09-03 22:35:15 +0200

    updated bootstrap

--- environments/linux/scripts/fedora/bootstrap.sh
+++ environments/linux/scripts/fedora/bootstrap.sh
@@ -13,16 +13,9 @@ then
     # Update all dependencies and install packages
     sudo dnf update -y
     
-    sudo dnf install -y git build-essential vim vim-gtk3 neovim wget curl htop lsof tilix tmux tty-clock \
-    fzf fasd zsh fish taskwarrior copyq ranger mc cmus httpie tree xsel samba-common exfat-utils \
-    nginx python python3 python3-pip ruby memcached docker docker-compose python3-pip pkg-config screenfetch \
-    libsecret-1-0 libsecret-1-dev atool highlight w3m mediainfo vim-gtk fonts-noto-color-emoji emacs cmake libvterm-dev
-    
-    echo "Configuration git credentials gnome"
-    pushd /usr/share/doc/git/contrib/credential/libsecret
-    sudo make
-    git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/
-    popd
+    sudo dnf install -y git vim wget curl htop lsof tmux tty-clock \
+    fasd zsh fish taskwarrior copyq ranger mc cmus httpie tree xsel \
+    atool highlight w3m mediainfo emacs
     
     # SYSCTL
     echo "setting sysctl to 9001!!!"
@@ -30,18 +23,11 @@ then
     echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 else
     # Update all dependencies and install packages
-    apt update -y
+    dnf update -y
     
-    apt install -y git build-essential vim neovim wget curl htop lsof tilix tmux tty-clock \
-    fzf fasd zsh taskwarrior copyq ranger mc cmus httpie tree xsel samba-common exfat-utils \
-    nginx python python3 python3-pip ruby memcached docker docker-compose python3-pip pkg-config screenfetch \
-    libsecret-1-0 libsecret-1-dev atool highlight w3m mediainfo vim-gtk fonts-noto-color-emoji emacs cmake libvterm-dev
-    
-    echo "Configuration git credentials gnome"
-    pushd /usr/share/doc/git/contrib/credential/libsecret
-    make
-    git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/
-    popd
+    sudo dnf install -y git vim wget curl htop lsof tmux tty-clock \
+    fasd zsh fish taskwarrior copyq ranger mc cmus httpie tree xsel \
+    atool highlight w3m mediainfo emacs
     
     # SYSCTL
     echo "setting sysctl to 9001!!!"
