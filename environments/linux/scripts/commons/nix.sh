437a59a7552438fe32aa36b0e37aecd15335d8ce
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2022-09-04 12:21:07 -0400

    Dont display nix packages

--- environments/linux/scripts/commons/nix.sh
+++ environments/linux/scripts/commons/nix.sh
@@ -6,7 +6,7 @@ sh <(curl -L https://nixos.org/nix/install) --no-daemon
 source ~/.nix-profile/etc/profile.d/nix.sh
 
 # search
-nix-env -qPa
+# nix-env -qPa
 # install
 nix-env -i starship vscodium neovim lazygit lazydocker fasd-unstable exa bat duf
 # remove
