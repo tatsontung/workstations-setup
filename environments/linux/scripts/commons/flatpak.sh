5288bfc21591b69c73f15f47b7c6b14098594d3b
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2022-09-23 15:59:24 +0200

    added more flatpak apps

--- environments/linux/scripts/commons/flatpak.sh
+++ environments/linux/scripts/commons/flatpak.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-flatpak install --user org.kde.dolphin
-flatpak install --user com.steamgriddb.steam-rom-manager
+# Dev tools
 flatpak install --user com.microsoft.Teams
 flatpak install --user com.visualstudio.code
 flatpak install --user us.zoom.Zoom
 flatpak install --user org.ferdium.Ferdium
-#flatpak install --user io.podman_desktop.PodmanDesktop
+flatpak install --user io.podman_desktop.PodmanDesktop
 flatpak install --user org.cryptomator.Cryptomator
+flatpak install --user org.freefilesync.FreeFileSync
+
+# Game tools
+flatpak install --user com.steamgriddb.steam-rom-manager
+flatpak install --user org.citra_emu.citra
+flatpak install --user org.yuzu_emu.yuzu
+flatpak install --user org.DolphinEmu.dolphin-emu
+flatpak install --user org.duckstation.DuckStation
+flatpak install --user net.pcsx2.PCSX2
+flatpak install --user org.ppsspp.PPSSPP
