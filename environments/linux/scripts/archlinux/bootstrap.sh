c0ac64e30801dfb40e41f7a67c3bb0b3f1c6694f
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2023-07-13 01:02:36 +0200

    Updated archlinux bootstrap script

--- environments/linux/scripts/archlinux/bootstrap.sh
+++ environments/linux/scripts/archlinux/bootstrap.sh
@@ -1,8 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 echo "Installation must have software for Archlinux workstation"
+sudo pacman -S --needed base-devel git openssh git curl wget zip unzip
+
+git clone https://aur.archlinux.org/paru.git
+pushd paru || exit
+makepkg -si
+popd || exit
+
 paru -Syu git kitty neovim-nightly-bin gvim emacs zsh fish lsof nginx copyq ranger mc stow \
-tmux httpie xsel fzf fasd wget curl python python-pip libsecret highlight atool w3m mediainfo \
-nfs-utils samba openssh exfatprogs zip unzip curl wget screenfetch \
-zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps
+	tmux httpie xsel fzf fasd wget curl python python-pip libsecret highlight atool w3m mediainfo \
+	nfs-utils samba exfatprogs screenfetch \
+	zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps
 
 sudo systemctl enable sshd
