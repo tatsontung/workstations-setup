2c6e444fbec7147f0960b4b08f14e4f7a990c616
Tat Son Tung NGUYEN <tatsontung.nguyen@gmail.com>
2023-08-04 00:07:00 +0200

    updated configuration for archlinux

--- environments/linux/scripts/archlinux/bootstrap.sh
+++ environments/linux/scripts/archlinux/bootstrap.sh
@@ -2,13 +2,8 @@
 echo "Installation must have software for Archlinux workstation"
 sudo pacman -S --needed base-devel git openssh git curl wget zip unzip
 
-git clone https://aur.archlinux.org/paru.git
-pushd paru || exit
-makepkg -si
-popd || exit
-
-paru -Syu git kitty neovim-nightly-bin gvim emacs zsh fish lsof nginx copyq ranger mc stow \
-	tmux httpie xsel fzf fasd wget curl python python-pip libsecret highlight atool w3m mediainfo \
+paru -Syu kitty neovim vim emacs zsh fish lsof nginx xclip copyq ranger mc stow \
+	tmux httpie xsel fzf fasd wget curl python python-pip \
 	nfs-utils samba exfatprogs screenfetch \
 	zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps
 
